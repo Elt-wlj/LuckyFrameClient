8de4131d36a5029f0c8d99fea359cf131f82d970
luobozz <437337961@qq.com>
2021-11-08 14:26:41 +0800

    change shell sh to bash

--- src/main/Resources/start_service.sh
+++ src/main/Resources/start_service.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 BASE_DIR="."
 LIB="${BASE_DIR}/lib"
 JAVA_OPTS=" -Xmx2048m -XX:PermSize=64m -XX:MaxPermSize=512m -XX:+PrintGCDateStamps -XX:+PrintGCDetails -Xloggc:logs/gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=1 -XX:GCLogFileSize=1024k -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=logs/mtdperf.hprof -server -Dfile.encoding=UTF-8"
